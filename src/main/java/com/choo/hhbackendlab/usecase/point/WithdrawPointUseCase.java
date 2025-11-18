cf53e67b98a39ad43379d4115799ea23c14e930b
chooHyun <hjchoo19950327@gmail.com>
2025-11-19 00:41:07 +0900

    Comment : 포인트 충전 및 출금 주석을 통해 추후 공부할 내용 작성

--- src/main/java/com/choo/hhbackendlab/usecase/point/WithdrawPointUseCase.java
+++ src/main/java/com/choo/hhbackendlab/usecase/point/WithdrawPointUseCase.java
@@ -15,8 +15,14 @@ public class WithdrawPointUseCase {
 
     private final PointWalletRepository pointWalletRepository;
 
+    /**
+     * 영속성 컨텍스트 -> Dirty Checking읕 통해 charge()메서드: 포인트 출금 및 사용
+     * @param userId
+     * @param amount
+     */
     @Transactional
     public void withdrawPoint(Long userId, int amount) {
+        //영속성 컨텍스트 스냅샷 저장
         PointWallet pointWallet = pointWalletRepository.findByUserIdWithLock(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. User ID: " + userId));
 
@@ -25,6 +31,7 @@ public class WithdrawPointUseCase {
             throw new IllegalStateException("잔액이 부족합니다. 현재 잔액: " + pointWallet.getBalance() + ", 요청 금액: " + amount);
         }
 
+        //Dirty Checking 자동 Update
         pointWallet.use(amount);
     }
 }
